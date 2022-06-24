2c7b9a52ac5ebde69df9b713ae28993b78102510
vladyslav.shmykov <vladyslav.shmykov@wtt-solutions.com>
2022-06-24 09:36:13 +0300

    111

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.scss';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+    const [quantity, setQuantity] = useState(0);
+
+    function increment() {
+        setQuantity(quantity + 1);
+    }
+
+    function decrement() {
+        setQuantity(quantity - 1);
+    }
+
+    return (
+        <div>
+            <h1>Counter: {quantity}</h1>
+            <button onClick={increment} className={'btn btn-success'}>Add</button>
+            <button onClick={decrement} className={'btn btn-danger'}>Remove</button>
+        </div>
+    );
 }
 
 export default App;
